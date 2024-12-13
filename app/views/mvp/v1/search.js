b4bf25e10d74fcbbdd32d5f0fb8ff9f19c18f52f
tarikjohnston1 <hi.tarik.johnston@nhsbsa.nhs.uk>
2024-12-13 10:57:00 +0000

    final change

--- app/views/mvp/v1/search.js
+++ app/views/mvp/v1/search.js
@@ -4,8 +4,19 @@ module.exports = router => {
 
   
   router.post(['/mvp/v1/search/'], (req, res, next) => {
-    const data = req.session.data
-    res.redirect('mrn')
+    const data = req.session.data;
+    var search = req.session.data['data.searchTerm'];
+
+
+    if(search == 'CHEDP.GB.2024.4450758' ) {
+        res.redirect('ched')
+      } 
+      else if(search == '24GB6T3HFCIZV1HAR9') {
+      res.redirect('mrn')
+    }
+    else if(search == '') {
+      res.redirect('search')
+    }
   })
 
 
