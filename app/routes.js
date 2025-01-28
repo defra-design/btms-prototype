9589cd14e45fed0964d031ba19220fec439d584e
tarikjohnston1 <hi.tarik.johnston@nhsbsa.nhs.uk>
2025-01-28 14:07:30 +0000

    update to mrn

--- app/routes.js
+++ app/routes.js
@@ -8,3 +8,4 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
 require('./views/mvp/v1/search')(router)
+require('./views/mvp/v2/search')(router)
---
+++ app/views/mvp/v2/search.js
@@ -0,0 +1,23 @@
+const _ = require('lodash')
+
+module.exports = router => {
+
+  
+  router.post(['/mvp/v2/search/'], (req, res, next) => {
+    const data = req.session.data;
+    var search = req.session.data['data.searchTerm'];
+
+
+    if(search == 'CHEDP.GB.2024.4450758' ) {
+        res.redirect('ched')
+      } 
+      else if(search == '24GB6T3HFCIZV1HAR9') {
+      res.redirect('mrn')
+    }
+    else {
+      res.redirect('search')
+    }
+  })
+
+
+}
