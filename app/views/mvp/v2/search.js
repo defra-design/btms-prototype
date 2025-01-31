113d47cb80b4574b41de648d14d202d1868d2bab
tarikjohnston1 <hi.tarik.johnston@nhsbsa.nhs.uk>
2025-01-31 10:18:35 +0000

    update to non-mrn

--- app/views/mvp/v2/search.js
+++ app/views/mvp/v2/search.js
@@ -20,7 +20,7 @@ module.exports = router => {
     else if(search == '24GBDX8QQ4WWFZNAR3') {
       res.redirect('mrn-try-auth')
     }
-    else if(search == '2025.5426583') {
+    else if(search == 'CHEDPP.GB.2025.5426583') {
       res.redirect('no-mrn')
     }
     else if(search == '25GB0P0TEP7CZCNAR6') {
