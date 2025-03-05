64e9ba75670435c70c733e3b5e7dc7d1eb53c8cc
tarikjohnston1 <hi.tarik.johnston@nhsbsa.nhs.uk>
2025-03-05 11:26:00 +0000

    update to new ched window

--- app/filters.js
+++ app/filters.js
@@ -8,4 +8,42 @@ const addFilter = govukPrototypeKit.views.addFilter
 
 
 // Add your filters here
+addFilter('chedTime', function() {
+    try {
+        const today = new Date();
+        // Add 2 days to the current date
+        const twoDaysLater = new Date(today.setDate(today.getDate() - 2));
+        // Subtract 3 hours from the current time
+        twoDaysLater.setHours(twoDaysLater.getHours() - 3);
 
+        // Format the date
+        const formattedDate = new Intl.DateTimeFormat('en-GB', { year: 'numeric', month: 'long', day: 'numeric' }).format(twoDaysLater);
+        // Format the time
+        const formattedTime = new Intl.DateTimeFormat('en-GB', { hour: '2-digit', minute: '2-digit' }).format(twoDaysLater);
+
+        // Combine date and time with a comma
+        return `${formattedDate}, ${formattedTime}`;
+    } catch (error) {
+        return error.message.split(':')[0];
+    }
+});
+
+addFilter('mrnTime', function() {
+    try {
+        const today = new Date();
+        // Add 2 days to the current date
+        const twoDaysLater = new Date(today.setDate(today.getDate() - 1));
+        // Subtract 3 hours from the current time
+        twoDaysLater.setHours(twoDaysLater.getHours() - 1);
+
+        // Format the date
+        const formattedDate = new Intl.DateTimeFormat('en-GB', { year: 'numeric', month: 'long', day: 'numeric' }).format(twoDaysLater);
+        // Format the time
+        const formattedTime = new Intl.DateTimeFormat('en-GB', { hour: '2-digit', minute: '2-digit' }).format(twoDaysLater);
+
+        // Combine date and time with a comma
+        return `${formattedDate}, ${formattedTime}`;
+    } catch (error) {
+        return error.message.split(':')[0];
+    }
+});
