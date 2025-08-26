c1c1d425e0de45553e1728c12528dc63d9d80e8d
tarikjohnston1 <hi.tarik.johnston@nhsbsa.nhs.uk>
2025-08-26 15:44:09 +0100

    update

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,8 +1,8 @@
 module.exports = {
 
   // Insert values here
-  'noMatch': 'The following CHED reference from the customs declaration cannot be found. Check the CHED reference on the customs declaration:<br><br>',
-  'noMatchContextual': 'This CHED reference cannot be found on the customs declaration. Please check that the reference is correct.',
+  'noMatch': 'This CHED reference cannot be found in IPAFFS. Please check that the reference is correct.<br><br>',
+  'noMatchContextual': 'This CHED reference cannot be found in IPAFFS. Please check that the reference is correct.',
   'Ipaffs': 'IPAFFS pre-notification (CHED) details',
   'Cds': 'Customs declaration details'
 
